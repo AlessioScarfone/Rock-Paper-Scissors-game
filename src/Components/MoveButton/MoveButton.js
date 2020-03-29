27ecb698dded7aa8c3422b7e6f96662fb80a1d87
Alessio Scarfone <alessioscarfone93@gmail.com>
2020-03-29 16:12:44 +0200

    init MoveButton [WIP]

---
+++ src/Components/MoveButton/MoveButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MoveButton.module.scss';
+
+const MoveButton = () => {
+    return (
+        <div className={styles.MoveButton}>
+            {/* TODO: to implement move button (Find bast way to pass an image or src) */}
+        </div>
+    );
+}
+
+export default MoveButton;
