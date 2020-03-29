37b487dcb27cdc8f853b172f8b7c591bedfe5d19
Alessio Scarfone <alessioscarfone93@gmail.com>
2020-03-29 16:14:57 +0200

    todo

--- src/Components/MoveButton/MoveButton.js
+++ src/Components/MoveButton/MoveButton.js
@@ -5,6 +5,9 @@ const MoveButton = () => {
     return (
         <div className={styles.MoveButton}>
             {/* TODO: to implement move button (Find bast way to pass an image or src) */}
+            {/* TODO: Move image on src */}
+            {/* "I create a folder called images in my source folder and put all my images there. And I can import them with say import imageName from '../images/imageName.png'" */}
+            {/* https://stackoverflow.com/questions/53190989/where-should-i-put-images-files-in-react-project */}
         </div>
     );
 }
