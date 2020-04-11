90158c622ac119d1a17cd57d6ffaa00aa3349ff0
Alessio Scarfone <alessioscarfone93@gmail.com>
2020-04-11 17:59:20 +0200

    modal backdrop

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -3,18 +3,22 @@ import styles from './Modal.module.scss'
 import close from '../../assets/images/icon-close.svg';
 import rules_basic from '../../assets/images/image-rules.svg';
 
-const Modal = ({onCloseHandler}) => {
+const Modal = ({ onCloseHandler }) => {
     return (
-        <div className={styles.Modal}>
-            <div className={styles.modalHeader}>
-                <h2 className={styles.title}>RULES</h2>
-                <img src={close} alt="close" className={[styles.closeIcon,styles.desktop].join(' ')} onClick={onCloseHandler}></img>
+        <>
+            <div className={styles.modalBackdrop}></div>
+            <div className={styles.Modal}>
+
+                <div className={styles.modalHeader}>
+                    <h2 className={styles.title}>RULES</h2>
+                    <img src={close} alt="close" className={[styles.closeIcon, styles.desktop].join(' ')} onClick={onCloseHandler}></img>
+                </div>
+
+                <img src={rules_basic} alt="rules" className={styles.rulesImg}></img>
+
+                <img src={close} alt="close" className={[styles.closeIcon, styles.mobile].join(' ')} onClick={onCloseHandler}></img>
             </div>
-
-            <img src={rules_basic} alt="rules" className={styles.rulesImg}></img>
-
-            <img src={close} alt="close" className={[styles.closeIcon,styles.mobile].join(' ')} onClick={onCloseHandler}></img>
-        </div>
+        </>
     );
 }
 
