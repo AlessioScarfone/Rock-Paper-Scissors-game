b22d7369082f53cfa568e8a26aa2b6f391ea896d
Alessio Scarfone <alessioscarfone93@gmail.com>
2020-04-19 18:22:04 +0200

    fix html validation in move button

--- src/Components/MoveButton/MoveButton.js
+++ src/Components/MoveButton/MoveButton.js
@@ -5,9 +5,9 @@ const MoveButton = ({ img, customClass = [], moveType = "none", onClick = () => 
     return (
         <div className={[styles.MoveButton, ...customClass].join(' ')} data-type={moveType}>
             <button className={styles.btn} onClick={onClick}>
-                <div className={styles.img}>
+                <span className={styles.img}>
                     {img ? <img className={styles.btnImg} src={img} alt="move"></img> : null}
-                </div>
+                </span>
             </button>
         </div>
     );
